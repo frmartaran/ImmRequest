f35b332e12c4feeb47ea49d5accff8de5153c747
juliette Ruchel <julietteru@gmail.com>
2020-03-30 23:28:35 -0300

    [WIP] Working on creating the first migration

---
+++ ImmRequest.DataAccess/Context/ContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImmRequest.DataAccess.Context
+{
+    public class ContextFactory
+    {
+
+        public static ImmDbContext GetMemoryContext(string name)
+        {
+            var builder = new DbContextOptionsBuilder<ImmDbContext>();
+            var options = GetMemoryConfig(name, builder);
+            return new ImmDbContext(options);
+        }
+
+        private static DbContextOptions<ImmDbContext> GetMemoryConfig(string name,
+        DbContextOptionsBuilder<ImmDbContext> builder)
+        {
+            builder.UseInMemoryDatabase(name);
+            return builder.Options;
+        }
+    }
+}
---
+++ ImmRequest.DataAccess/Context/ImmDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using ImmRequest.Domain;
+using ImmRequest.Domain.Fields;
+using ImmRequest.Domain.UserManagement;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace ImmRequest.DataAccess.Context
+{
+    public class ImmDbContext : DbContext
+    {
+        public ImmDbContext(DbContextOptions<ImmDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Administrator> Administrators { get; set; }
+
+        public virtual DbSet<Session> Sessions { get; set; }
+
+        public virtual DbSet<CitizenRequest> CitizenRequests { get; set; }
+
+        public virtual DbSet<Area> Areas { get; set; }
+
+        public virtual DbSet<Topic> Topics { get; set; }
+
+        public virtual DbSet<TopicType> TopicTypes { get; set; }
+
+        public virtual DbSet<NumberField> NumberFields { get; set; }
+
+        public virtual DbSet<TextField> TextFields { get; set; }
+
+        public virtual DbSet<DateTimeField> DateTimeFields { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TextField>()
+                        .Property(e => e.RangeValues)
+                        .HasConversion(
+                            v => JsonConvert.SerializeObject(v),
+                            v => JsonConvert.DeserializeObject<List<string>>(v));
+
+        }
+    }
+}
