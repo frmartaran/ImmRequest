044092c234b6ca4f6371d1f03763b9313cff5520
juliette Ruchel <julietteru@gmail.com>
2020-04-01 20:41:44 -0300

    added repository interface

--- ImmRequest.DataAccess.Interfaces/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ImmRequest.DataAccess.Interfaces
-{
-    public class Class1
-    {
-    }
-}
---
+++ ImmRequest.DataAccess.Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImmRequest.DataAccess.Interfaces
+{
+    public interface IRepository<T>
+    where T: class
+    {
+        void Insert(T id);
+
+        T Update(T objectToUpdate);
+
+        T Get(long id);
+
+        ICollection<T> GetAll();
+
+        void Delete(long id);
+
+        bool Exists(long id);
+        
+        void Save();
+
+
+
+        
+    }
+}
