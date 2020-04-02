367a3c1d49ebe27af203bb839d091ba74e494491
juliette Ruchel <julietteru@gmail.com>
2020-04-02 18:27:11 -0300

    Created Files for Session Repository

---
+++ ImmRequest.DataAccess.Tests/SessionRepositoryTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.DataAccess.Tests
+{
+    public class SessionRepositoryTest
+    {
+
+    }
+}
---
+++ ImmRequest.DataAccess/Repostories/SessionRepository.cs
@@ -0,0 +1,53 @@
+﻿using ImmRequest.DataAccess.Context;
+using ImmRequest.DataAccess.Interfaces;
+using ImmRequest.Domain.UserManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.DataAccess.Repostories
+{
+    public class SessionRepository : IRepository<Session>
+    {
+        private ImmDbContext Context { get; set; }
+
+        public SessionRepository(ImmDbContext context)
+        {
+            Context = context;
+        }
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Session Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Session> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Session objectToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Session Update(Session objectToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
