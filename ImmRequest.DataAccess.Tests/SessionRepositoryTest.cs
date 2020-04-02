0c1a0f45fc72980db8ae74c9a316059f8c0ecdcc
juliette Ruchel <julietteru@gmail.com>
2020-04-02 18:43:04 -0300

    [RED] InsertTest

--- ImmRequest.DataAccess.Tests/SessionRepositoryTest.cs
+++ ImmRequest.DataAccess.Tests/SessionRepositoryTest.cs
@@ -1,11 +1,58 @@
-﻿using System;
+﻿using ImmRequest.DataAccess.Context;
+using ImmRequest.DataAccess.Repostories;
+using ImmRequest.Domain.UserManagement;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ImmRequest.DataAccess.Tests
 {
+    [TestClass]
     public class SessionRepositoryTest
     {
+        private Session session;
+        private Administrator administrator;
+        private ImmDbContext context;
+        private SessionRepository repository;
 
+        [TestInitialize]
+        public void SetUp()
+        {
+
+            administrator = new Administrator
+            {
+                UserName = "임 재범",
+                PassWord = "1234"
+            };
+
+            session = new Session
+            {
+                AdministratorInSession = administrator,
+                Token = Guid.NewGuid()
+            };
+        }
+
+        private void CreateRepository(string name)
+        {
+            context = ContextFactory.GetMemoryContext(name);
+            repository = new SessionRepository(context);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            CreateRepository("InsertTest");
+            context.Administrators.Add(administrator);
+            context.SaveChanges();
+
+            session.AdministratorId = administrator.Id;
+            repository.Insert(session);
+
+            var sessionCount = context.Sessions.Count();
+            Assert.AreEqual(1, sessionCount);
+        }
     }
 }
