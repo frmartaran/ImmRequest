fce2c8f11844f6fbbe07f913c00000e4fb7e3be9
juliette Ruchel <julietteru@gmail.com>
2020-04-05 15:32:28 -0300

    [RED] InsertTest

--- ImmRequest.DataAccess.Tests/AreaRepositoryTest.cs
+++ ImmRequest.DataAccess.Tests/AreaRepositoryTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ImmRequest.DataAccess.Context;
 using ImmRequest.DataAccess.Repositories;
 using ImmRequest.Domain;
@@ -34,6 +35,16 @@ namespace ImmRequest.DataAccess.Tests
         {
             context = ContextFactory.GetMemoryContext(name);
             repository = new AreaRepository(context);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            CreateRepository("InsertTest");
+            repository.Insert(area);
+
+            var areaInDb = context.Areas.FirstOrDefault();
+            Assert.IsNotNull(areaInDb);
         }
     }
 }
