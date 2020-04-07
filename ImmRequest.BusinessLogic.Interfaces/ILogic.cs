48ad86679a9beb9dd335769785080ebcf04747e7
juliette Ruchel <julietteru@gmail.com>
2020-04-07 20:45:11 -0300

    Created Logic Interace

---
+++ ImmRequest.BusinessLogic.Interfaces/ILogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImmRequest.BusinessLogic.Interfaces
+{
+    public interface ILogic<T>
+        where T : class
+    {
+        void Create(T objectToCreate);
+        void Update(T objectToUpdate);
+        void Delete(int id);
+        T Get(int Id);
+        ICollection<T> GetAll();
+
+    }
+}
