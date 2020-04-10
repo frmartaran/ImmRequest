4c7e24c6fe9e54876d876a4e4887e9144dc399e9
juliette Ruchel <julietteru@gmail.com>
2020-04-10 18:35:00 -0300

    [GREEN] Added IsValid Tests using memory context

--- ImmRequest.BusinessLogic.Tests/ValidatorTest.cs/AdministratorValidatorTest.cs
+++ ImmRequest.BusinessLogic.Tests/ValidatorTest.cs/AdministratorValidatorTest.cs
@@ -1,4 +1,6 @@
+using ImmRequest.DataAccess.Context;
 using ImmRequest.DataAccess.Interfaces;
+using ImmRequest.DataAccess.Repositories;
 using ImmRequest.Domain.UserManagement;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -23,7 +25,7 @@ namespace ImmRequest.BusinessLogic.Tests
         }
 
         [TestMethod]
-        public void IsValidAdmin()
+        public void IsValidAdminWithUniqueEmail()
         {
             var mockRepository = new Mock<IRepository<Administrator>>(MockBehavior.Strict);
             mockRepository.Setup(m => m.Exists(It.IsAny<Administrator>()))
@@ -34,5 +36,55 @@ namespace ImmRequest.BusinessLogic.Tests
             Assert.IsTrue(isValid);
             mockRepository.VerifyAll();
         }
+
+        [TestMethod]
+        public void IsNotValidAdminWithRepeatedEmail()
+        {
+            var mockRepository = new Mock<IRepository<Administrator>>(MockBehavior.Strict);
+            mockRepository.Setup(m => m.Exists(It.IsAny<Administrator>()))
+                .Returns(true);
+            var validator = new AdministratorValidator(mockRepository.Object);
+            var isValid = validator.IsValid(administrator);
+
+            Assert.IsFalse(isValid);
+            mockRepository.VerifyAll();
+        }
+
+        [TestMethod]
+        public void IsValidAdminWithUniqueEmailUsingContext()
+        {
+            var context = ContextFactory.GetMemoryContext("Unique Email");
+            var repository = new AdministratorRepository(context);
+            repository.Insert(administrator);
+            var validator = new AdministratorValidator(repository);
+            var newAdmin = new Administrator
+            {
+                Email = "youngjae@email.com",
+                UserName = "최 영재",
+                PassWord = "1234"
+            };
+
+            var isValid = validator.IsValid(newAdmin);
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void IsNotValidWithRepeatedEmailUsingContext()
+        {
+            var context = ContextFactory.GetMemoryContext("Repeated Email");
+            var repository = new AdministratorRepository(context);
+            repository.Insert(administrator);
+            var validator = new AdministratorValidator(repository);
+            var newAdmin = new Administrator
+            {
+                Email = "jackson@wang.com",
+                UserName = "최 영재",
+                PassWord = "1234"
+            };
+
+            var isValid = validator.IsValid(newAdmin);
+            Assert.IsFalse(isValid);
+        }
+
     }
 }
