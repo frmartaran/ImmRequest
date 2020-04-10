55249851f6db9c6fd90ffea91e9610447a19912d
juliette Ruchel <julietteru@gmail.com>
2020-04-10 19:21:05 -0300

    [GREEN] IsNotValidWithoutToken

--- ImmRequest.BusinessLogic/Validators/SessionValidator.cs
+++ ImmRequest.BusinessLogic/Validators/SessionValidator.cs
@@ -1,4 +1,6 @@
-﻿using ImmRequest.BusinessLogic.Interfaces;
+﻿using ImmRequest.BusinessLogic.Exceptions;
+using ImmRequest.BusinessLogic.Interfaces;
+using ImmRequest.BusinessLogic.Resources;
 using ImmRequest.DataAccess.Interfaces;
 using ImmRequest.Domain.UserManagement;
 using System;
@@ -11,12 +13,20 @@ namespace ImmRequest.BusinessLogic.Validators
     {
         private IRepository<Session> Repository { get; set; }
 
+        private const string Token_PropertyName = "Token";
+
         public SessionValidator(IRepository<Session> repository)
         {
             Repository = repository;
         }
         public bool IsValid(Session objectToValidate)
         {
+            if (Guid.Empty == objectToValidate.Token)
+            {
+                var errorMessage = string.Format(BusinessResource.ValidationError_IsEmpty, 
+                    Token_PropertyName);
+                throw new ValidationException(errorMessage);
+            }
             return true;
         }
     }
