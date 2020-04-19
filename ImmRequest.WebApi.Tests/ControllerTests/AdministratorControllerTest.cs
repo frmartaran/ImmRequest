a6dd2caabdb85bf9e50d9c5280c06dc44fec3111
juliette Ruchel <julietteru@gmail.com>
2020-04-19 15:33:15 -0300

    [RED] toDomainTest (In Administrator Controller since I realized I need this model first)

---
+++ ImmRequest.WebApi.Tests/ControllerTests/AdministratorControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using ImmRequest.Domain.UserManagement;
+using ImmRequest.WebApi.Models.UserManagement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.WebApi.Tests.ControllerTests
+{
+    [TestClass]
+    public class AdministratorControllerTest
+    {
+        private AdministratorModel model;
+        private Administrator administrator;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            model = new AdministratorModel
+            {
+                Email = "example@example.com",
+                Password = "1234",
+                Username = "Example"
+            };
+
+            administrator = new Administrator
+            {
+                Email = "another@example.com",
+                PassWord = "1235",
+                UserName = "Another Example"
+            };
+        }
+
+        [TestMethod]
+        public void ToDomainTest()
+        {
+            var admin = model.ToDomain();
+            Assert.AreEqual(model.Email, admin.Email);
+            Assert.AreEqual(model.Username, admin.UserName);
+            Assert.AreEqual(model.Password, admin.PassWord);
+
+        }
+    }
+}
