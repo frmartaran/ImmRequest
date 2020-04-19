65d54a0a48f17d57471085f86934722f0df87578
juliette Ruchel <julietteru@gmail.com>
2020-04-19 19:02:56 -0300

    [GREEN]

--- ImmRequest.WebApi/Helpers/CurrentSessionInfo.cs
+++ ImmRequest.WebApi/Helpers/CurrentSessionInfo.cs
@@ -9,9 +9,12 @@ namespace ImmRequest.WebApi.Helpers
 {
     public class CurrentSessionInfo : IContextHelper
     {
+        private const string Authorization_Header = "Authorization";
         public Guid GetAuthorizationHeader(HttpContext context)
         {
-            throw new NotImplementedException();
+            var tokenString = context.Request.Headers[Authorization_Header];
+            Guid.TryParse(tokenString, out var token);
+            return token;
         }
     }
 }
