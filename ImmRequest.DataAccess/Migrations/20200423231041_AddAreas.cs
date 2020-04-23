11f2403fd95e239729bb1d9ed998c1af39695dd3
juliette Ruchel <julietteru@gmail.com>
2020-04-23 20:35:06 -0300

    Added migrations to insert Areas and Topics Default values

---
+++ ImmRequest.DataAccess/Migrations/20200423231041_AddAreas.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImmRequest.DataAccess.Migrations
+{
+    public partial class AddAreas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO dbo.Areas (Name) VALUES ('Espacios Publicos y Calles')");
+            migrationBuilder.Sql("INSERT INTO dbo.Areas (Name) VALUES ('Limpieza')");
+            migrationBuilder.Sql("INSERT INTO dbo.Areas (Name) VALUES ('Saneamiento')");
+            migrationBuilder.Sql("INSERT INTO dbo.Areas (Name) VALUES ('Transporte')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM dbo.Areas");
+        }
+    }
+}
