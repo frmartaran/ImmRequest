28870541082247492a1bc7df8868e765fe74eda2
juliette Ruchel <julietteru@gmail.com>
2020-04-24 19:46:39 -0300

    [REFACTOR]

--- ImmRequest.Domain/Fields/DateTimeField.cs
+++ ImmRequest.Domain/Fields/DateTimeField.cs
@@ -83,9 +83,14 @@ namespace ImmRequest.Domain.Fields
 
         public override bool ValidateRangeValues()
         {
+            IsStartGreaterThenEnd();
+            return true;
+        }
+
+        private void IsStartGreaterThenEnd()
+        {
             if (Start.CompareTo(End) > 0)
                 throw new DomainValidationException(DomainResource.FieldRange_StartGreaterThanEnd);
-            return true;
         }
     }
 }
