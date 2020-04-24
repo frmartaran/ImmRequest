bd332acce5109a7166eb8ff60eb90a6b27c96212
juliette Ruchel <julietteru@gmail.com>
2020-04-24 17:46:41 -0300

    [RED] IsValidTest

---
+++ ImmRequest.BusinessLogic.Tests/ValidatorTest/TopicTypeValidatorTest.cs
@@ -0,0 +1,53 @@
+﻿using ImmRequest.BusinessLogic.Validators;
+using ImmRequest.Domain;
+using ImmRequest.Domain.Fields;
+using ImmRequest.Domain.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.BusinessLogic.Tests.ValidatorTest
+{
+    [TestClass]
+    public class TopicTypeValidatorTest
+    {
+        private Area area;
+        private Topic topic;
+        private TopicType type;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            area = new Area
+            {
+                Name = "Area"
+            };
+
+            topic = new Topic
+            {
+                Name = "Topic",
+                Area = area
+            };
+
+            type = new TopicType
+            {
+                Name = "Type",
+                ParentTopic = topic,
+                AllFields = new List<BaseField>()
+            };
+
+        }
+
+        [TestMethod]
+        public void IsValidTest()
+        {
+            var validator = new TopicTypeValidator();
+            var isValid = validator.IsValid(type);
+
+            Assert.IsTrue(isValid);
+        }
+
+
+    }
+}
---
+++ ImmRequest.BusinessLogic/Validators/TopicTypeValidator.cs
@@ -0,0 +1,16 @@
+﻿using ImmRequest.BusinessLogic.Interfaces;
+using ImmRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.BusinessLogic.Validators
+{
+    public class TopicTypeValidator : IValidator<TopicType>
+    {
+        public bool IsValid(TopicType objectToValidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
