749b6db4848470a675c01dfa27d2500d33f9566b
juliette Ruchel <julietteru@gmail.com>
2020-04-25 11:29:36 -0300

    [GREEN]

--- ImmRequest.BusinessLogic/Logic/TopicTypeLogic.cs
+++ ImmRequest.BusinessLogic/Logic/TopicTypeLogic.cs
@@ -30,7 +30,7 @@ namespace ImmRequest.BusinessLogic.Logic
 
         public void Delete(long id)
         {
-            throw new NotImplementedException();
+            Repository.Delete(id);
         }
 
         public TopicType Get(long Id)
