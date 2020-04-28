aca358cac4082df382df1a9fe73361dd069275a5
juliette Ruchel <julietteru@gmail.com>
2020-04-28 16:32:51 -0300

    [RED]

---
+++ ImmRequest.WebApi.Tests/ControllerTests/TopicTypeControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using ImmRequest.Domain;
+using ImmRequest.Domain.Fields;
+using ImmRequest.WebApi.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.WebApi.Tests.ControllerTests
+{
+    [TestClass]
+    public class TopicTypeControllerTest
+    {
+        private Area area;
+        private Topic topic;
+        private TopicType type;
+        private NumberField numberField;
+        private DateTimeField datesField;
+        private TextField textField;
+
+        [TestMethod]
+        public void SetUp()
+        {
+            numberField = new NumberField
+            {
+                Name = "Age Range",
+                RangeStart = 1,
+                RangeEnd = 25
+            };
+
+            datesField = new DateTimeField
+            {
+                Name = "Year of birth",
+                Start = new DateTime(1995, 1, 1),
+                End = new DateTime(2019, 1, 1)
+            };
+
+            textField = new TextField
+            {
+                Name = "Other Requirements",
+                RangeValues = new List<string>
+                {
+                    "Credencial"
+                }
+            };
+
+            area = new Area
+            {
+                Name = "Area Name"
+            };
+
+            topic = new Topic
+            {
+                Name = "Ne Name"
+            };
+
+            type = new TopicType
+            {
+                Name = "Type Name",
+                AllFields = new List<BaseField>
+                {
+                    numberField,
+                    datesField,
+                    textField
+                }
+            };
+
+        }
+
+        [TestMethod]
+        public void TypeToModelTest()
+        {
+            var model = TypeModel.ToModel(type);
+
+            Assert.AreEqual(model.Name, type.Name);
+        }
+
+
+
+    }
+}
---
+++ ImmRequest.WebApi/Models/TypeModel.cs
@@ -0,0 +1,24 @@
+﻿using ImmRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImmRequest.WebApi.Models
+{
+    public class TypeModel : Model<TopicType, TypeModel>
+    {
+        public string Name { get; set; }
+
+
+        public override TypeModel SetModel(TopicType entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TopicType ToDomain()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
