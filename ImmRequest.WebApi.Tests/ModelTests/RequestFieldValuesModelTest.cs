6ffb19cf103176543dfa88ab996ac0c4bbd05ff6
frmartaran <frmartaran@gmail.com>
2020-04-29 19:39:11 -0300

    [RED] RequestFieldValuesModel.SetModel, RequestFieldValuesModel.ToDomain

---
+++ ImmRequest.WebApi.Tests/ModelTests/RequestFieldValuesModelTest.cs
@@ -0,0 +1,54 @@
+﻿using ImmRequest.Domain;
+using ImmRequest.WebApi.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImmRequest.WebApi.Tests.ModelTests
+{
+    [TestClass]
+    public class RequestFieldValuesModelTest
+    {
+        private RequestFieldValues requestFieldValues;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            requestFieldValues = new RequestFieldValues
+            {
+                Id = 1,
+                ParentCitizenRequestId = 1,
+                FieldId = 1,
+                Value = "Credencial"
+            };
+        }
+
+        [TestMethod]
+        public void SetModel()
+        {
+            var requestFieldValuesModel = new RequestFieldValuesModel();
+            requestFieldValuesModel.SetModel(requestFieldValues);
+
+            Assert.AreEqual(requestFieldValues.Id, requestFieldValuesModel.Id);
+            Assert.AreEqual(requestFieldValues.FieldId, requestFieldValuesModel.FieldId);
+            Assert.AreEqual(requestFieldValues.ParentCitizenRequestId, requestFieldValuesModel.ParentCitizenRequestId);
+            Assert.AreEqual(requestFieldValues.Value, requestFieldValuesModel.Value);
+        }
+
+        [TestMethod]
+        public void ToDomain()
+        {
+            var requestFieldValuesModel = new RequestFieldValuesModel
+            {
+                ParentCitizenRequestId = 1,
+                FieldId = 1,
+                Value = "Credencial"
+            };
+            var requestFieldValues = requestFieldValuesModel.ToDomain();
+
+            Assert.AreEqual(requestFieldValuesModel.FieldId, requestFieldValues.FieldId);
+            Assert.AreEqual(requestFieldValuesModel.ParentCitizenRequestId, requestFieldValues.ParentCitizenRequestId);
+            Assert.AreEqual(requestFieldValuesModel.Value, requestFieldValues.Value);
+        }
+    }
+}
