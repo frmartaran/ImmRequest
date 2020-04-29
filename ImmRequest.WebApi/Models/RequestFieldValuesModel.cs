cfd53671963d3bfdb49b7589204deee0695e2572
frmartaran <frmartaran@gmail.com>
2020-04-29 19:39:34 -0300

    [GREEN] RequestFieldValuesModel.SetModel, RequestFieldValuesModel.ToDomain

---
+++ ImmRequest.WebApi/Models/RequestFieldValuesModel.cs
@@ -0,0 +1,38 @@
+﻿using ImmRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImmRequest.WebApi.Models
+{
+    public class RequestFieldValuesModel : Model<RequestFieldValues, RequestFieldValuesModel>
+    {
+        public long Id { get; set; }
+
+        public long ParentCitizenRequestId { get; set; }
+
+        public long FieldId { get; set; }
+
+        public string Value { get; set; }
+
+        public override RequestFieldValuesModel SetModel(RequestFieldValues entity)
+        {
+            Id = entity.Id;
+            ParentCitizenRequestId = entity.ParentCitizenRequestId;
+            FieldId = entity.FieldId;
+            Value = entity.Value;
+            return this;
+        }
+
+        public override RequestFieldValues ToDomain()
+        {
+            return new RequestFieldValues
+            {
+                ParentCitizenRequestId = ParentCitizenRequestId,
+                FieldId = FieldId,
+                Value = Value
+            };
+        }
+    }
+}
