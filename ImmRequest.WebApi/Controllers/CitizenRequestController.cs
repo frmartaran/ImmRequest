650ee10d414fb217e1c887c58de66987ce0db5ce
frmartaran <frmartaran@gmail.com>
2020-04-29 23:55:09 -0300

    [GREEN] CitizenRequestController.CreateCitizenRequest

---
+++ ImmRequest.WebApi/Controllers/CitizenRequestController.cs
@@ -0,0 +1,45 @@
+﻿using ImmRequest.BusinessLogic.Exceptions;
+using ImmRequest.BusinessLogic.Interfaces;
+using ImmRequest.Domain;
+using ImmRequest.Domain.Enums;
+using ImmRequest.WebApi.Filters;
+using ImmRequest.WebApi.Helpers;
+using ImmRequest.WebApi.Models;
+using ImmRequest.WebApi.Resources;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ImmRequest.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("CorsPolicy")]
+    public class CitizenRequestController: ControllerBase
+    {
+        private ILogic<CitizenRequest> CitizenRequestLogic { get; set; }
+
+        public CitizenRequestController(ILogic<CitizenRequest> CitizenRequestLogic)
+        {
+            this.CitizenRequestLogic = CitizenRequestLogic;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCitizenRequest([FromBody] CitizenRequestModel requestModel)
+        {
+            try
+            {
+                if(requestModel == null)
+                {
+                    return BadRequest(WebApiResource.CitizenRequest_EmptyRequestMessage);
+                }
+                var request = requestModel.ToDomain();
+                CitizenRequestLogic.Create(request);
+                return Ok(WebApiResource.CitizenRequest_CreatedMessage);
+            }
+            catch (ValidationException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+    }
+}
