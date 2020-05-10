96159ffe69855a01943f8debdaa387ebd903045a
frmartaran <frmartaran@gmail.com>
2020-05-10 10:57:03 -0300

    [GREEN] AreaModel.SetModel

---
+++ ImmRequest.WebApi/Models/AreaModel.cs
@@ -0,0 +1,26 @@
+﻿using ImmRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImmRequest.WebApi.Models
+{
+    public class AreaModel : Model<Area, AreaModel>
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<TopicModel> Topics { get; set; }
+
+        public override AreaModel SetModel(Area entity)
+        {
+            Id = entity.Id;
+            Name = entity.Name;
+            Topics = TopicModel
+                .ToModel(entity.Topics)
+                .ToList();
+            return this;
+        }
+    }
+}
