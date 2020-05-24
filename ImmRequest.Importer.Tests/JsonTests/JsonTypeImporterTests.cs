55eec0559a4da3bad0c166d1a22c38f9c108ba5f
juliette Ruchel <julietteru@gmail.com>
2020-05-23 21:38:17 -0300

    [RED]

--- ImmRequest.Importer.Tests/JsonTests/JsonTypeImporterTests.cs
+++ ImmRequest.Importer.Tests/JsonTests/JsonTypeImporterTests.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ImmRequest.Importer.Importers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ImmRequest.Importer.Tests.JsonTests
@@ -8,6 +10,15 @@ namespace ImmRequest.Importer.Tests.JsonTests
     [TestClass]
     public class JsonTypeImporterTests
     {
+        [TestMethod]
+        public void SuccessfulImport()
+        {
+            var path = $"{TestConstants.JsonPath}\\ImportType.json";
+            var importer = new JsonTypeImporter(path);
+            var types = importer.Import();
 
+            Assert.AreEqual(1, types.Count);
+            Assert.AreEqual(4, types.First().Fields.Count);
+        }
     }
 }
