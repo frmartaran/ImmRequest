5ef22a1917256066be745db9cd0582d73719fd73
juliette Ruchel <julietteru@gmail.com>
2020-05-24 21:51:05 -0300

    [GREEN]

--- ImmRequest.Importer.Tests/JsonTests/JsonImporterTests.cs
+++ ImmRequest.Importer.Tests/JsonTests/JsonImporterTests.cs
@@ -1,4 +1,5 @@
 using ImmRequest.Importer.Importers;
+using ImmRequest.Importer.Interfaces.Domain;
 using ImmRequest.Importer.Interfaces.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
@@ -69,6 +70,45 @@ namespace ImmRequest.Importer.Tests
             var path = $"{TestConstants.JsonPath}\\EmptyJson.json";
             var importer = new JsonTypeImporter(path);
             var types = importer.Import();
+        }
+
+        [TestMethod]
+        public void SuccessfulImportOfMultipleTypes()
+        {
+            var path = $"{TestConstants.JsonPath}\\MultipleTypes.json";
+            var importer = new JsonTypeImporter(path);
+            var types = importer.Import();
+
+            Assert.AreEqual(3, types.Count);
+
+            var type1 = types.First();
+
+            Assert.AreEqual(4, type1.Fields.Count);
+
+            var numberField = type1.Fields.FirstOrDefault(f => f.DataType == DataType.Number);
+            var textField = type1.Fields.FirstOrDefault(f => f.DataType == DataType.Text);
+            var dateTimeField = type1.Fields.FirstOrDefault(f => f.DataType == DataType.DateTime);
+            var boolField = type1.Fields.FirstOrDefault(f => f.DataType == DataType.Bool);
+
+            Assert.IsNotNull(numberField);
+            Assert.IsNotNull(textField);
+            Assert.IsNotNull(dateTimeField);
+            Assert.IsNotNull(boolField);
+
+            var type2 = types.Skip(1).First();
+            Assert.AreEqual(2, type2.Fields.Count);
+            var numberField2 = type2.Fields.FirstOrDefault(f => f.DataType == DataType.Number);
+            var textField2 = type2.Fields.FirstOrDefault(f => f.DataType == DataType.Text);
+            var dateTimeField2 = type2.Fields.FirstOrDefault(f => f.DataType == DataType.DateTime);
+            var boolField2 = type2.Fields.FirstOrDefault(f => f.DataType == DataType.Bool);
+
+            Assert.IsNull(numberField2);
+            Assert.IsNotNull(textField2);
+            Assert.IsNotNull(dateTimeField2);
+            Assert.IsNull(boolField2);
+
+            var type3 = types.Skip(2).First();
+            Assert.AreEqual(0, type3.Fields.Count);
         }
 
 
