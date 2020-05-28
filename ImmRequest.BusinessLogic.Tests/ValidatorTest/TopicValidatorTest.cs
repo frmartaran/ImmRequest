09b4b3863158b395da80cf0d7568a35d1544799a
juliette Ruchel <julietteru@gmail.com>
2020-05-27 22:16:58 -0300

    [RED]

---
+++ ImmRequest.BusinessLogic.Tests/ValidatorTest/TopicValidatorTest.cs
@@ -0,0 +1,39 @@
+﻿using ImmRequest.BusinessLogic.Validators;
+using ImmRequest.DataAccess.Interfaces;
+using ImmRequest.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.BusinessLogic.Tests.ValidatorTest
+{
+    [TestClass]
+    public class TopicValidatorTest
+    {
+
+        private Topic topic;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            topic = new Topic
+            {
+                Area = new Area(),
+                Name = "Topic",
+                Types = new List<TopicType>()
+            };
+        }
+
+        [TestMethod]
+        public void IsValidTopic()
+        {
+            var mockRepository = new Mock<IRepository<Topic>>();
+            var validator = new TopicValidator(mockRepository.Object);
+            var isValid = validator.IsValid(topic);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
---
+++ ImmRequest.BusinessLogic/Validators/TopicValidator.cs
@@ -0,0 +1,23 @@
+﻿using ImmRequest.BusinessLogic.Interfaces;
+using ImmRequest.DataAccess.Interfaces;
+using ImmRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.BusinessLogic.Validators
+{
+    public class TopicValidator : IValidator<Topic>
+    {
+        private IRepository<Topic> Repository { get; set; }
+
+        public TopicValidator(IRepository<Topic> repository)
+        {
+            Repository = repository;
+        }
+        public bool IsValid(Topic objectToValidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
