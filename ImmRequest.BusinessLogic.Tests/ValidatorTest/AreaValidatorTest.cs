47ba6697176cbf4163044c36d6917c5129238769
juliette Ruchel <julietteru@gmail.com>
2020-05-27 21:37:38 -0300

    [RED]

---
+++ ImmRequest.BusinessLogic.Tests/ValidatorTest/AreaValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿using ImmRequest.BusinessLogic.Interfaces;
+using ImmRequest.BusinessLogic.Validators;
+using ImmRequest.DataAccess.Interfaces;
+using ImmRequest.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.BusinessLogic.Tests.ValidatorTest
+{
+    [TestClass]
+    public class AreaValidatorTest
+    {
+
+        private Area area;
+        [TestInitialize]
+        public void Setup()
+        {
+            area = new Area
+            {
+                Name = "Area",
+                Topics = new List<Topic> { new Topic() }
+            };
+        }
+
+        [TestMethod]
+        public void AreaIsValid()
+        {
+            var mockRepository = new Mock<IRepository<Area>>();
+            var validator = new AreaValidator(mockRepository.Object);
+            var isValid = validator.IsValid(area);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
---
+++ ImmRequest.BusinessLogic/Validators/AreaValidator.cs
@@ -0,0 +1,23 @@
+﻿using ImmRequest.BusinessLogic.Interfaces;
+using ImmRequest.DataAccess.Interfaces;
+using ImmRequest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmRequest.BusinessLogic.Validators
+{
+    public class AreaValidator : IValidator<Area>
+    {
+        private IRepository<Area> Repository { get; set; }
+
+        public AreaValidator(IRepository<Area> repository)
+        {
+            Repository = repository;
+        }
+        public bool IsValid(Area objectToValidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
