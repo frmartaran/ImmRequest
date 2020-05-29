359844f212930c765f476b2244417dbc6a808074
juliette Ruchel <julietteru@gmail.com>
2020-05-29 19:02:39 -0300

    [REFACTOR]

--- ImmRequest.BusinessLogic.Tests/ImporterTests/ImporterTests.cs
+++ ImmRequest.BusinessLogic.Tests/ImporterTests/ImporterTests.cs
@@ -1,7 +1,11 @@
-﻿using ImmRequest.BusinessLogic.Logic.ImporterLogic;
+﻿using ImmRequest.BusinessLogic.Helpers.Inputs;
+using ImmRequest.BusinessLogic.Interfaces;
+using ImmRequest.BusinessLogic.Logic.ImporterLogic;
 using ImmRequest.BusinessLogic.Validators;
+using ImmRequest.DataAccess.Interfaces;
 using ImmRequest.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,7 +18,16 @@ namespace ImmRequest.BusinessLogic.Tests.ImporterTests
         [TestMethod]
         public void GetImporterOptionsTest()
         {
-            var logic = new ImporterLogic();
+            var inputs = new AreaImporterInput
+            (
+                areaRepository: new Mock<IRepository<Area>>().Object,
+                topicRepository: new Mock<IRepository<Topic>>().Object,
+                areaValidator: new Mock<IValidator<Area>>().Object,
+                topicValidator: new Mock<IValidator<Topic>>().Object,
+                typeValidator: new Mock<IValidator<TopicType>>().Object
+            );
+
+            var logic = new ImporterLogic(inputs);
             var allImporterNames = logic.GetImporterOptions();
             var jsonTypeImporter = "Json Area Importer";
             var xmlTypeImporter = "Xml Area Importer";
@@ -22,6 +35,25 @@ namespace ImmRequest.BusinessLogic.Tests.ImporterTests
             Assert.IsTrue(allImporterNames.Contains(jsonTypeImporter));
             Assert.IsTrue(allImporterNames.Contains(xmlTypeImporter));
 
+        }
+
+        [TestMethod]
+        public void GetsImporterOfAnotherAssembly()
+        {
+            var inputs = new AreaImporterInput
+            (
+                areaRepository: new Mock<IRepository<Area>>().Object,
+                topicRepository: new Mock<IRepository<Topic>>().Object,
+                areaValidator: new Mock<IValidator<Area>>().Object,
+                topicValidator: new Mock<IValidator<Topic>>().Object,
+                typeValidator: new Mock<IValidator<TopicType>>().Object
+            );
+
+            var logic = new ImporterLogic(inputs);
+            var allImporterNames = logic.GetImporterOptions();
+            var otherImporter = "Test Importer";
+
+            Assert.IsTrue(allImporterNames.Contains(otherImporter));
         }
     }
 }
--- ImmRequest.BusinessLogic/Logic/ImporterLogic/ImporterLogic.cs
+++ ImmRequest.BusinessLogic/Logic/ImporterLogic/ImporterLogic.cs
@@ -1,7 +1,9 @@
 ﻿using ImmRequest.BusinessLogic.Extentions;
+using ImmRequest.BusinessLogic.Helpers.Inputs;
 using ImmRequest.BusinessLogic.Interfaces;
 using ImmRequest.BusinessLogic.Validators;
 using ImmRequest.DataAccess.Interfaces;
+using ImmRequest.Domain;
 using ImmRequest.Importer.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -14,26 +16,45 @@ namespace ImmRequest.BusinessLogic.Logic.ImporterLogic
 {
     public class ImporterLogic : IImporterLogic
     {
-        private const string IMPORTER_ASSEMBLY_PATH = @"~\..\ImmRequest.Importer.dll";
-        private const string DATA_ACCESS_ASSEMBLY_PATH = @"`\..\ImmRequest.DataAccess.dll";
+        private const string IMPORTER_ASSEMBLY_PATH = @"~\..\Importers";
+        private IRepository<Area> AreaRepository { get; set; }
+        private IRepository<Topic> TopicRepository { get; set; }
+        private IValidator<Area> AreaValidator { get; set; }
+        private IValidator<Topic> TopicValidator { get; set; }
+        private IValidator<TopicType> TopicTypeValidator { get; set; }
 
+        public ImporterLogic(AreaImporterInput input)
+        {
+            AreaRepository = input.AreaRepository;
+            TopicRepository = input.TopicRepository;
+            AreaValidator = input.AreaValidator;
+            TopicValidator = input.TopicValidator;
+            TopicTypeValidator = input.TopicTypeValidator;
+        }
 
         public List<string> GetImporterOptions()
         {
             var normalizedPath = NormalizePath(IMPORTER_ASSEMBLY_PATH);
-            var assembly = Assembly.LoadFile(normalizedPath);
-            var importers = assembly.GetTypes()
-                .Where(t => t.GetInterfaces()
-                    .Any(i => i.IsGenericType
-                        && i.GetGenericTypeDefinition() == typeof(IEntityImporter<,>)
+            var allImporters = new List<string>();
+            var allFiles = Directory.GetFiles(normalizedPath, "*.dll");
+            foreach (var dll in allFiles)
+            {
+                var assembly = Assembly.LoadFile(dll);
+                var importers = assembly.GetTypes()
+                    .Where(t => t.GetInterfaces()
+                        .Any(i => i.IsGenericType
+                            && i.GetGenericTypeDefinition() == typeof(IEntityImporter<,>)
+                        )
                     )
-                )
-                .Where(t => !t.GetTypeInfo().IsAbstract)
-                .ToList();
-            var allNames = importers
-                .Select(im => im.Name.SplitByCamelCase())
-                .ToList();
-            return allNames;
+                    .Where(t => !t.GetTypeInfo().IsAbstract)
+                    .ToList();
+                var allNames = importers
+                    .Select(im => im.Name.SplitByCamelCase())
+                    .ToList();
+                allImporters.AddRange(allNames);
+
+            }
+            return allImporters;
         }
 
         private static string NormalizePath(string pathToNormalize)
