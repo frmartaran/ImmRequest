b409433321d860624b44b50949f428b3d314caac
juliette Ruchel <julietteru@gmail.com>
2020-06-06 17:49:08 -0300

    fixed css

--- ImmRequestClientApp/src/app/components/type-editor/type-editor.component.ts
+++ ImmRequestClientApp/src/app/components/type-editor/type-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { BaseField } from 'src/app/models/models';
 
 @Component({
   selector: 'app-type-editor',
@@ -8,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 
 export class TypeEditorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackBarService: SnackbarService) { }
+
+  public action: string;
+
+  public id: number;
+
+  public name: string;
+
+  public fields: BaseField[];
 
   ngOnInit() {
   }
